4826546a1f35eb9c9a98395e64bd25296dd2ac14
Abigail Gooding <ablygo@gmail.com>
2020-05-04 13:12:13 -0700

    stuff

--- src/Numeric/Module.hs
+++ src/Numeric/Module.hs
@@ -9,10 +9,22 @@ import Numeric
 -- |
 -- Laws:
 --
--- (x /= zero) => (magnitude x * normalize x = x)
 class (Zero (Scalar a)) => Module a where
     type Scalar a :: Type
     (*) :: Scalar a -> a -> a
+
+instance Module UTCTime where
+    type Scalar UTCTime 
+
+-- (magnitude x * normalize x = x)
+-- normalize zero = zero
+-- (x /= zero) => magnitude (normalize x) = one
+-- 
+-- vector-space norm
+class (Module a) => Euclidean a where
+    -- the magnitude can be defined on its own for vector spaces, and then
+    -- normalize can be defined as 'normalize x = recip (norm x) * x' for
+    -- non-zero values, and 'normalize zero = zero' otherwise
     normalize :: a -> a
     magnitude :: a -> Scalar a
 
