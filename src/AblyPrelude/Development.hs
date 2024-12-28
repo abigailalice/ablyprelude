e26d9d180fc5845ccc6d95ace553456f500b74b7
Abigail Gooding <abigailalicegooding@gmail.com>
2024-12-27 20:49:16 -0800

    Add table pretty printer

--- src/AblyPrelude/Development.hs
+++ src/AblyPrelude/Development.hs
@@ -4,6 +4,8 @@
 module AblyPrelude.Development
     ( module X
     , undefined
+    , textDims
+    , pad
     , traceM
     , debugTraceM
     , deadCode
@@ -26,6 +28,7 @@ module AblyPrelude.Development
     ) where
 
 import Prelude hiding (undefined)
+import Control.Lens
 import qualified Prelude 
 
 import Debug.Trace as X hiding (traceM)
@@ -33,6 +36,7 @@ import qualified Debug.Trace as DT
 import qualified Data.Aeson as DA
 import qualified Data.List as DL
 import qualified Data.Text as DT
+import qualified Data.Text.Lens as DTL
 import qualified GHC.Stack as GS
 
 import Text.Show.Pretty (pPrint)
@@ -45,6 +49,48 @@ import qualified Data.Aeson.Encode.Pretty as DAEP
 import qualified Language.Haskell.TH.Syntax as LHTS
 import qualified Data.FileEmbed as DF
 
+textDims :: DT.Text -> (Int, Int)
+textDims xs =
+    let ys :: [DT.Text]
+        ys = DT.lines xs
+    in (maximum $ fmap textWidth $ ys, length ys)
+
+textWidth :: DT.Text -> Int
+textWidth = sumOf (DTL.unpacked . folded . from enum . to go)
+  where
+    go :: Int -> Int
+    go n
+        -- cjk unified ideographs
+        | 0x4e00 <= n && n <= 0x9fff = 2
+        | 0x3400 <= n && n <= 0x4dbf = 2
+        | 0x20000 <= n && n <= 0x2a6df = 2
+        | 0x2a700 <= n && n <= 0x2b73f = 2
+        -- hangul
+        | 0xac00 <= n && n <= 0xd7af = 2
+        -- hiragana and katakana
+        | 0x3040 <= n && n <= 0x309f = 2
+        | 0x30a0 <= n && n <= 0x30ff = 2
+        -- fullwidth forms
+        | 0xff01 <= n && n <= 0xff60 = 2
+        | 0xffe0 <= n && n <= 0xffe6 = 2
+        -- cjk compatibility ideographs
+        | 0xf900 <= n && n <= 0xfaff = 2
+        -- symbols and punctuation
+        | 0x3000 <= n && n <= 0x303f = 2
+        | otherwise = 1
+
+padWidth :: Int -> DT.Text -> DT.Text
+padWidth n xs =
+    let m = textWidth xs
+    in DT.replicate (n - m) " " <> xs
+
+pad :: (Int, Int) -> DT.Text -> DT.Text
+pad (x, y) xs =
+    let xs', xs'' :: [DT.Text]
+        xs' = DT.lines xs
+        xs'' = xs' <> replicate (y - length xs') ""
+    in DT.intercalate "\n" $ fmap (padWidth x) $ xs''
+
 -- |@pShowTable@ is a helper function which takes a grid of text and prints it
 -- as a table, with padding so that cells are lined in columns, and a bordered
 -- header for the first row
@@ -52,21 +98,20 @@ pShowTable :: Bool -> [[DT.Text]] -> DT.Text
 pShowTable b xs
     = DT.intercalate "\n"
     $ (if b then addHeaderBorder else id)
-    $ fmap (DT.intercalate " | ")
+    $ fmap (DT.intercalate "   ")
     $ DL.transpose
-    $ zip lengths xs' <&> \(n, column) -> column <&> \cell -> DT.justifyLeft n ' ' cell
+    $ zip lengths xs' <&> \(n, column) -> column <&> \cell -> padWidth n cell
   where
-    (<&>) = flip fmap
 
     addHeaderBorder :: [DT.Text] -> [DT.Text]
     addHeaderBorder [] = []
-    addHeaderBorder (y : ys) = y : DT.replicate (DT.length y) "-" : ys
+    addHeaderBorder (y : ys) = y : DT.replicate (textWidth y) "-" : ys
 
     xs' :: [[DT.Text]]
     xs' = DL.transpose xs
 
     lengths :: [Int]
-    lengths = fmap (maximum . fmap DT.length) $ xs'
+    lengths = fmap (maximum . fmap textWidth) $ xs'
 
 embedFile :: [Char] -> LHTS.Q LHTS.Exp
 embedFile x = do
